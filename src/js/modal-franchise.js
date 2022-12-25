77ca723d787659526fa32b0fb51e4cfb5e9c1161
SerhiiShymko <shimko456@gmail.com>
2022-12-25 20:59:13 +0200

    container

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -1,6 +1,6 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal- franchise-open]'),
+    openModalBtn: document.querySelector('[data-modal-franchise-open]'),
     closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
     modal: document.querySelector('[data-modal-franchise]'),
   };
