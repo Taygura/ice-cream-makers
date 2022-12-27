4d2722aaa7faca318c355afebb6e19cdbefd7677
SerhiiShymko <shimko456@gmail.com>
2022-12-27 22:42:06 +0200

    modal_prod

--- src/js/modal-product.js
+++ src/js/modal-product.js
@@ -1,47 +1,49 @@
 // Modal-cream
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal__open--product-cream]"),
-      closeModalBtn: document.querySelector("[data-modal__close--product-cream]"),
-      modal: document.querySelector("[data-modal__product-cream]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal__open--product-cream]'),
+    closeModalBtn: document.querySelector('[data-modal__close--product-cream]'),
+    modal: document.querySelector('[data-modal__product-cream]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
 
-// Modal-coffee 
+// Modal-coffee
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal__open--product-coffee]"),
-      closeModalBtn: document.querySelector("[data-modal__close--product-coffee]"),
-      modal: document.querySelector("[data-modal__product-coffee]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal__open--product-coffee]'),
+    closeModalBtn: document.querySelector(
+      '[data-modal__close--product-coffee]'
+    ),
+    modal: document.querySelector('[data-modal__product-coffee]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
 
 // Modal-milk
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal__open--product-milk]"),
-      closeModalBtn: document.querySelector("[data-modal__close--product-milk]"),
-      modal: document.querySelector("[data-modal__product-milk]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal__open--product-milk]'),
+    closeModalBtn: document.querySelector('[data-modal__close--product-milk]'),
+    modal: document.querySelector('[data-modal__product-milk]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
